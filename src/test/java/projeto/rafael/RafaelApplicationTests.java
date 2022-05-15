4be9c88e6b672bc1598e4fa7a0fd1cea222a35f7
Rafael Campos Mendonça <rafael.campos.mendonca@hotmail.com>
2022-05-14 21:19:24 -0300

    seding project

---
+++ src/test/java/projeto/rafael/RafaelApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.rafael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RafaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
